e2a30f017b009d00b642a5014dea3d4f85b75f08
John Kesler <John_Kesler@caryacademy.org>
2019-02-14 17:26:52 -0500

    improved vector class to be more useful

--- arduino/data_server.py
+++ arduino/data_server.py
@@ -13,12 +13,19 @@ class Vector:
         self.compound = [self.endPoint[0]-self.originPoint[0], self.endPoint[1]-self.originPoint[1]]
         self.slope = self.compound[1]/self.compound[0] if self.compound[0] != 0 else 0
         self.yIntercept = self.slope*(-self.originPoint[0])+self.originPoint[1]
+        self.vectorDetected = not (self.originPoint == [129, 0] and self.endPoint == [254, 188])
+        self.realSlope = self.compound[0] != 0
 
     def __str__(self):
-        return "({}), ({}), y-int: {}".format(", ".join(list(map(str, self.originPoint))), ", ".join(list(map(str, self.endPoint))), str(self.yIntercept))
+        if self.realSlope and self.vectorDetected:
+            return "({}), y-int: {}".format(", ".join(list(map(str, self.compound))), str(self.yIntercept))
+        elif self.vectorDetected:
+            return "({}), infinite slope".format(", ".join(list(map(str, self.compound))))
+        else:
+            return "No vector detected"
 
-    def solveForX(self):
-        pass
+    def getYFromX(self, x):
+        return self.slope*x+self.yIntercept
 
 
 serialConnnection = serial.Serial(
