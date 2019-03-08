fa06b1d39973927bacedef222423949270493eed
Sheridan Page <Sheridan_Page@caryacademy.org>
2019-03-08 14:59:50 -0500

    possibly fixed analog pressure sensor

--- Kirkland/components/analog_pressure_sensor.py
+++ Kirkland/components/analog_pressure_sensor.py
@@ -7,18 +7,18 @@ class AnalogPressureSensor():
     CALIBRATION_PRESSURE = 110
 
     def __init__(self, port):
-        self.port = port
+        self.analog = AnalogInput(port)
     
     def get_raw_output(self):
-        return AnalogInput.getVoltage(self.port)
+        return self.analog.getVoltage()
 
     def get_uncalibrated_pressure_psi(self):
         """return pressure without calibrating (assuming Vcc is 5.0V)"""
-        return 250 * (AnalogInput.getVoltage(self.port) / 5.0) - 25
+        return 250 * (self.analog.getVoltage() / 5.0) - 25
 
     def get_pressure_psi(self):
         """return calibrated pressure (take Vcc variability into account)"""
-        return 250 * (AnalogInput.getVoltage(self.port) / self.normalized_voltage) - 25
+        return 250 * (self.analog.getVoltage() / self.normalized_voltage) - 25
     
     def calibrate_pressure(self):
         """calibrate the sensor to account for Vcc variability"""
@@ -29,4 +29,4 @@ class AnalogPressureSensor():
         2. press calibration button (TBD)     
         """
 
-        self.normalized_voltage = float(AnalogInput.getVoltage(self.port) / ((0.004 * self.CALIBRATION_PRESSURE) + 0.1))
+        self.normalized_voltage = float(self.analog.getVoltage() / ((0.004 * self.CALIBRATION_PRESSURE) + 0.1))
